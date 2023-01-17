903ef45376c81cab5fc82744d7a35b095825a79e
Kaleab Bayih <kaleabbyh@gmail.com>
2023-01-18 00:30:09 +0300

    commited

--- noruReservation/urls.py
+++ noruReservation/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('places.urls'))
