ce335c0b6b6784d7c6716f8983982aac2be9fdac
Kaleab Bayih <kaleabbyh@gmail.com>
2023-01-18 00:29:23 +0300

    commited

--- places/models.py
+++ places/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
 class Customer(models.Model):
     user = models.OneToOneField(
         User, null=True, blank=True, on_delete=models.CASCADE)
