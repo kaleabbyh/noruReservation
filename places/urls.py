dea5ed76e53bf52a562fe37e386985ad3548c5f5
Kaleab Bayih <kaleabbyh@gmail.com>
2023-08-06 00:22:48 +0300

    Update urls.py

--- places/urls.py
+++ places/urls.py
@@ -3,3 +3,10 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home')
 ]
+
+
+# from django.urls import path
+# from . import views
+# urlpatterns = [
+#     path('',views.home,name='home')
+# ]
