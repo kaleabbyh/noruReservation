9422cd8152a28e9ce61138982f2cdaf11c4bb139
Kaleab Bayih <kaleabbyh@gmail.com>
2023-08-21 20:55:56 +0300

    Update urls.py

--- noruReservation/urls.py
+++ noruReservation/urls.py
@@ -1,8 +1,8 @@
 
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf.urls.static import static
-# from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
